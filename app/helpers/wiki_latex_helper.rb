8a9227bfbf5c2af16242da83c11f5bc1f47c6597
Andrew Sednev <exantrius@gmail.com>
2015-08-03 12:59:34 +0300

    tracing

--- app/helpers/wiki_latex_helper.rb
+++ app/helpers/wiki_latex_helper.rb
@@ -1,5 +1,5 @@
 require 'digest/sha2'
-require	'tempfile'
+require 'tempfile'
 
 module WikiLatexHelper
   def render_image_tag(image_name, source)
@@ -9,33 +9,37 @@ module WikiLatexHelper
   def render_image_block(image_name, source, wiki_name)
     render_to_string :template => 'wiki_latex/macro_block', :layout => false, :locals => {:name => image_name, :source => source, :wiki_name => wiki_name}
   end
-	class Macro
-		def initialize(view, source)
-		  @view = view
-		  @view.controller.extend(WikiLatexHelper)
-			source.gsub!(/<br \/>/,"")
-			source.gsub!(/<\/?p>/,"")
-			name = Digest::SHA256.hexdigest(source)
-			if !WikiLatex.find_by_image_id(name)
-				@latex = WikiLatex.new(:source => source, :image_id => name)
-				@latex.save
-			end
-			@latex = WikiLatex.find_by_image_id(name)
-		end
+    class Macro
+        def initialize(view, source)
+          Rails.logger.info 'MACRO INIT Start'
+          @view = view
+          @view.controller.extend(WikiLatexHelper)
+            source.gsub!(/<br \/>/,"")
+            source.gsub!(/<\/?p>/,"")
+            name = Digest::SHA256.hexdigest(source)
+            if !WikiLatex.find_by_image_id(name)
+                @latex = WikiLatex.new(:source => source, :image_id => name)
+                @latex.save
+            end
+            @latex = WikiLatex.find_by_image_id(name)
+          Rails.logger.info 'MACRO INIT End'
+        end
 
-		def render()
-		  if @latex
-		    @view.controller.render_image_tag(@latex.image_id, @latex.source)
-		  else
-		    @view.controller.render_image_tag("error", "error")
-		  end
-		end
-		def render_block(wiki_name)
-		  if @latex
-		    @view.controller.render_image_block(@latex.image_id, @latex.source, wiki_name)
-		  else
-		    @view.controller.render_image_block("error", "error", wiki_name)
-		  end
-		end
-	end
+        def render()
+          Rails.logger.info 'MACRO RENDER Start'
+          if @latex
+            @view.controller.render_image_tag(@latex.image_id, @latex.source)
+          else
+            @view.controller.render_image_tag("error", "error")
+          end
+          Rails.logger.info 'MACRO RENDER End'
+        end
+        def render_block(wiki_name)
+          if @latex
+            @view.controller.render_image_block(@latex.image_id, @latex.source, wiki_name)
+          else
+            @view.controller.render_image_block("error", "error", wiki_name)
+          end
+        end
+    end
 end
